c875297409ec3bf3ed0936f7de2fec1644d78d1a
KPBAEK <KPBAEK@sbmkorea.com>
2014-06-24 15:38:54 +0900

    .

---
+++ application/views/admin/container.php
@@ -0,0 +1,36 @@
+<?php 
+include("/include/admin/top.php");
+?>
+
+
+<body>
+<div id="main">
+	
+	<table>
+	<tr><td style="width:220px;vertical-align: top">
+		<div id="left" style="width: 200px;">
+<?php include("/include/admin/left.php");?>
+		</div>
+	</td>
+	<td style="width:800px;align:center; vertical-align:middle">
+		<div id="body">
+<?php $this->load->view('/admin/content'); ?>
+		</div>	
+	</td>
+	</tr>
+	<tr><td colspan=2>
+<div id="footer">
+<?php include("/include/admin/footer.php");?>
+</div>
+	</td>
+	</tr>
+	</table>
+
+</div>
+
+
+
+</body>
+
+</html>
+
