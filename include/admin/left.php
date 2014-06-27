831f8d51b25871ca55ac66868b7cb1c9eabd6821
KPBAEK <KPBAEK@sbmkorea.com>
2014-06-27 18:32:35 +0900

    .

--- include/admin/left.php
+++ include/admin/left.php
@@ -11,16 +11,16 @@
 					</li>
 					<li><span class="folder">주문서</span>
 						<ul>
-							<li><span class="file"><a href="/admin/order?countrytabs=0">부품</a></span></li>
-							<li><span class="file"><a href="/admin/order?countrytabs=1">장비</a></span></li>
+							<li><span class="file"><a href="/admin/order?countrytabs=0">장비</a></span></li>
+						<li><span class="file"><a href="/admin/order?countrytabs=1">부품</a></span></li>
 						</ul>
 					</li>
 					<li><span class="folder">사내문서</span>
 						<ul>
-							<li><span class="file"><a href="/admin/docs?countrytabs=0">부품출고</a></span></li>
-							<li><span class="file"><a href="/admin/docs?countrytabs=1">생산의뢰서</a></span></li>
-							<li><span class="file"><a href="/admin/docs?countrytabs=2">출고전표</a></span></li>
-						</ul>
+							<li><span class="file"><a href="/admin/docs?countrytabs=0">생산의뢰서</a></span></li>
+							<li><span class="file"><a href="/admin/docs?countrytabs=1">출고전표</a></span></li>
+							<li><span class="file"><a href="/admin/docs?countrytabs=2">부품출고의뢰서</a></span></li>
+							</ul>
 					</li>
 					<li><span class="folder">외부발송문서</span>
 						<ul>
@@ -33,7 +33,7 @@
 			</li>
 			<li><span class="folder">Product</span>
 				<ul>
-					<li><span class="file">부품관리</span></li>
+					<li><span class="file"><a href="/admin/product?countrytabs=0">부품관리</a></span></li>
 					</li>
 				</ul>
 			</li>
