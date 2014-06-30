cafb1c54be63b5ac5275288a0323314cbd02ddf8
KPBAEK <KPBAEK@sbmkorea.com>
2014-06-30 15:21:31 +0900

    .

--- application/views/admin/product/tab01.php
+++ application/views/admin/product/tab01.php
@@ -10,37 +10,55 @@
 	<script src="/lib/js/jqgrid/js/jquery.jqGrid.min.js" type="text/javascript"></script>	
 	<script src="/lib/js/themes/jquery-ui.custom.min.js" type="text/javascript"></script>
 </head>
+<div id="searchDiv" style="display:none">
 <form name="searchForm">
 <input type="text" name="page">
 <a href="javascript:gridReload();" id="search">page</a>
 </form>
+</div>
 <table id="list2"></table>
 <div id="pager2"></div>
+<div id="myDiv">
+<form name="viewForm">
+<input type="text" id="id">
+<input type="text" id="invdate">
+</form>
+</div>
 
 
 <script type="text/javascript">
 
+	var targetUrl = "/admin/product/listPart";
 	jQuery("#list2").jqGrid({
-	   	url:'/test/main/server',
-		datatype: "json",
-	   	colNames:['Inv No','Date', 'Client', 'Amount','Tax','Total','Notes'],
+	   	//url:'/test/main/server',
+	   	url:'/admin/product/listPart',
+	   	datatype: "json",
+	   	//colNames:['Inv No','Date', 'Client', 'Amount','Tax','Total','Notes'],
+	   	colNames:['Model', 'S/N', 'CODE', 'Part Name', 'IMAGE', 'Remark', 'Recommend', 'Spare Part', 'Wear Parts',  'Without Warranty'],
+   	              //, '(1CIS)', '(2CIS)', 'Q(Per 1Unit)', 'Order Price', 'Amount'
 	   	colModel:[
-	   		{name:'id',index:'id', width:55, align:"right"},
-	   		{name:'invdate',index:'invdate', width:90},
-	   		{name:'name',index:'name asc, invdate', width:100},
-	   		{name:'amount',index:'amount', width:80, align:"right"},
-	   		{name:'tax',index:'tax', width:80, align:"right"},		
-	   		{name:'total',index:'total', width:80,align:"right"},		
-	   		{name:'note',index:'note', width:150, sortable:false}		
-	   	],
-        mtype: "POST",
+	   		{name:'id',index:'id', width:70, align:"right"},
+	   		{name:'id',index:'invdate', width:70},
+	   		{name:'id',index:'name asc, invdate', width:70},
+	   		{name:'id',index:'amount', width:70, align:"right"},
+	   		{name:'id',index:'tax', width:70, align:"right"},		
+	   		{name:'id',index:'total', width:70,align:"right"},		
+	   		{name:'id',index:'note', width:70, sortable:false},		
+	   		{name:'id',index:'note', width:70, sortable:false},		
+	   		{name:'id',index:'note', width:70, sortable:false},		
+	   		{name:'id',index:'note', width:70, sortable:false}		
+		],
+        onSelectRow: function(id) {
+            view_detail("#list2",id);
+        },
+		mtype: "POST",
 	   	rowNum:10,
 	   	rowList:[10,20,30],
 	   	pager: '#pager2',
 	    viewrecords: true,
 	    autowidth: false,
 	    width:950,
-	    height:220,
+	    height:230,
 	    sortname: 'id',
 	    sortorder: "desc",
 	    caption:"부품관리"
@@ -50,7 +68,38 @@
 
     function gridReload() {
         var page = document.searchForm.page.value;
-        jQuery("#list2").jqGrid('setGridParam', {url:"/test/main/server",page:page}).trigger("reloadGrid");
+        jQuery("#list2").jqGrid('setGridParam', {url:targetUrl,page:page}).trigger("reloadGrid");
+    }
+
+    function view_detail(list, id) {
+        var chk_data = jQuery(list).jqGrid('getRowData',id);
+        var targetUrl = '/admin/product/viewPart?id=' + chk_data.id;
+        $.getJSON(targetUrl, function(result){
+            $.each(result, function(i, field){
+            	document.getElementById("id").value = field.id;
+                document.getElementById("invdate").value = result['viewPart']['invdate'];
+            });
+	    });
+    }
+    
+    function view_detail2(list, id) {
+        var chk_data = jQuery(list).jqGrid('getRowData',id);
+        targetUrl = "/admin/product/viewPart?id=" + chk_data.id;
+        $.ajax({
+            url: targetUrl,
+            type: 'GET',
+            dataType: 'json',
+            timeout: 1000,
+            error: function() {
+                alert('Error loading targetUrl');
+            },
+            success: function(result) {
+                $.each(result, function(i, field){
+                	document.getElementById("id").value = field.id;
+                    document.getElementById("invdate").value = result['viewPart']['invdate'];
+                });
+            }
+        });
     }
     
     var timeoutHnd;
