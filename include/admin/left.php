6619a8584116f0ce7386dea40e441ab22dbaa2df
KPBAEK <KPBAEK@sbmkorea.com>
2014-07-10 12:17:22 +0900

    .

--- include/admin/left.php
+++ include/admin/left.php
@@ -11,22 +11,22 @@
 					</li>
 					<li><span class="folder">주문서</span>
 						<ul>
-							<li><span class="file"><a href="/admin/order?countrytabs=0">장비</a></span></li>
-						<li><span class="file"><a href="/admin/order?countrytabs=1">부품</a></span></li>
+							<li><span class="file"><a href="/admin/order?ordertabs=0">장비</a></span></li>
+						<li><span class="file"><a href="/admin/order?ordertabs=1">부품</a></span></li>
 						</ul>
 					</li>
 					<li><span class="folder">사내문서</span>
 						<ul>
-							<li><span class="file"><a href="/admin/docs?countrytabs=0">생산의뢰서</a></span></li>
-							<li><span class="file"><a href="/admin/docs?countrytabs=1">출고전표</a></span></li>
-							<li><span class="file"><a href="/admin/docs?countrytabs=2">부품출고의뢰서</a></span></li>
+							<li><span class="file"><a href="/admin/docs?doctabs=0">생산의뢰서</a></span></li>
+							<li><span class="file"><a href="/admin/docs?doctabs=1">출고전표</a></span></li>
+							<li><span class="file"><a href="/admin/docs?doctabs=2">부품출고의뢰서</a></span></li>
 							</ul>
 					</li>
 					<li><span class="folder">외부발송문서</span>
 						<ul>
-							<li><span class="file"><a href="/admin/outer?countrytabs=0">Proforma invoice</a></span></li>
-							<li><span class="file"><a href="/admin/outer?countrytabs=1">INVOICE</a></span></li>
-							<li><span class="file"><a href="/admin/outer?countrytabs=2">PACKING LIST</a></span></li>
+							<li><span class="file"><a href="/admin/outer?outertabs=0">Proforma invoice</a></span></li>
+							<li><span class="file"><a href="/admin/outer?outertabs=1">INVOICE</a></span></li>
+							<li><span class="file"><a href="/admin/outer?outertabs=2">PACKING LIST</a></span></li>
 						</ul>
 					</li>
 				</ul>
