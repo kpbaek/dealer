10c2880e2828caf0572b8ff1ac45dc67cd66b5da
KPBAEK <KPBAEK@sbmkorea.com>
2014-07-14 11:24:10 +0900

    .

---
+++ application/views/admin/order/listDtlOrder.php
@@ -0,0 +1,97 @@
+<?php
+$sidx = "id"; // get index row - i.e. user click to sort
+if(isset($_REQUEST["sidx"])){
+	$sidx = $_REQUEST["sidx"];
+}
+$sord = "desc"; 
+if(isset($_REQUEST["sord"])){
+	$sord = $_REQUEST["sord"];
+}
+$page = "1";
+if(isset($_REQUEST["page"])){
+	$page = $_REQUEST["page"];
+}
+$limit = "10"; // get how many rows we want to have into the grid
+if(isset($_REQUEST["rows"])){
+	$limit = $_REQUEST["rows"];
+}
+log_message('debug', "start.........");
+
+//search param
+$searchId = "";
+if(isset($_REQUEST["id"])){
+	$searchId = $_REQUEST["id"];
+}
+log_message('debug', "id:" + $searchId);
+
+// include db config
+include_once("/config.php");
+
+
+// set up DB
+$db = mysql_connect(PHPGRID_DBHOST, PHPGRID_DBUSER, PHPGRID_DBPASS);
+mysql_select_db(PHPGRID_DBNAME);
+
+$start = $limit*$page - $limit; // do not put $limit*($page - 1)
+#$SQL = "SELECT a.id, a.invdate, b.name, a.amount,a.tax,a.total,a.note FROM invheader a, clients b 
+$SQL = "SELECT id as no, 'type' as type, '0' as price, '0' as qty, '0' as amount, 'tester' invdate
+		FROM invheader"; 
+$SQL_WHERE = " WHERE client_id=". $searchId ; 
+$SQL_ORDER = "		ORDER BY "
+		 . $sidx . " " . $sord . " LIMIT " . $start . "," . $limit;
+$SQL = $SQL . $SQL_WHERE . $SQL_ORDER;
+$result = mysql_query( $SQL ) or die("Couldn t execute query.".mysql_error());
+
+$SQL_CNT = "SELECT COUNT(*) AS count FROM invheader a";
+$SQL_CNT = $SQL_CNT . $SQL_WHERE;
+$cntResult = mysql_query($SQL_CNT);
+$cntRow = mysql_fetch_array($cntResult,MYSQL_ASSOC);
+$count = $cntRow['count'];
+
+if( $count >0 ) {
+	$total_pages = ceil($count/$limit);
+} else {
+	$total_pages = 0;
+}
+if ($page > $total_pages) $page=$total_pages;
+
+/**
+$responce = array('page'=>$page
+				 ,'total'=>$total_pages
+				 ,'records'=>$count
+				 ,'rows' => array(array('id'=>'1','name'=>'1','invdate'=>'1','name'=>'2')
+				 		         ,array('id'=>'2','name'=>'2','invdate'=>'2','name'=>'2')
+				 	)
+);
+#$responce['page'] = 2;
+#$responce['rows'][1]['id'] = 3;
+#echo $responce['rows'][1]['id'];
+*/
+
+$responce['page'] = $page;
+$responce['total'] = $total_pages;
+$responce['records'] = $count;
+
+$i=0;
+$qtytot = 0;
+$amttot = 0;
+$link = "";
+while($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
+	$link = "<input type=button value='edit'>";
+	$responce['rows'][$i]['no'] = $row['no'];
+	$responce['rows'][$i]['cell'] = array($row['no'],$row['type'],$row['qty'],$row['amount'],$row['invdate'],$link);
+	
+//	$qtytot += $row['qty'];
+//	$amttot += $row['amount'];
+	$qtytot += 0;
+	$amttot += 0;
+	
+#    echo $row['id'];
+    $i++;
+}  
+#$responce['userdata']['qty'] = $qtytot;
+#$responce['userdata']['amount'] = $amttot;
+#$responce['userdata']['total'] = 'Totals:';
+
+echo json_encode($responce);
+?>
