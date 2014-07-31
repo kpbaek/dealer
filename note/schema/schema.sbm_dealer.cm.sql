119e582494055910f6bc28de0d17c1888a3126ae
KPBAEK <KPBAEK@sbmkorea.com>
2014-07-31 11:46:40 +0900

    .

--- note/schema/schema.sbm_dealer.cm.sql
+++ note/schema/schema.sbm_dealer.cm.sql
@@ -1,71 +1,61 @@
 CREATE TABLE `cm_auth_grp` (
-  `auth_grp_cd` varchar(2) NOT NULL,
-  `auth_grp_nm` varchar(50) NOT NULL,
-  `auth_grp_dscrnm` varchar(200) DEFAULT NULL,
+  `auth_grp_cd` varchar(2) NOT NULL COMMENT '권한그룹코드',
+  `auth_grp_nm` varchar(50) NOT NULL COMMENT '권한그룹명',
+  `auth_grp_dscrt` varchar(200) DEFAULT NULL COMMENT '권한그룹설명',
   PRIMARY KEY (`auth_grp_cd`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='권한그룹';
 
 CREATE TABLE `cm_cd_attr` (
-  `cd_seq` int(4) NOT NULL,
-  `atcd` varchar(4) NOT NULL,
-  `atcd_nm` varchar(100) NOT NULL,
-  `atcd_dscrnm` varchar(200) DEFAULT NULL,
-  `use_yn` char(1) NOT NULL DEFAULT 'Y',
-  `ord_num` smallint(5) DEFAULT NULL,
-  `crt_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cd_seq` int(4) NOT NULL COMMENT '코드순번',
+  `atcd` varchar(4) NOT NULL COMMENT '속성코드',
+  `atcd_nm` varchar(100) NOT NULL COMMENT '속성코드명',
+  `atcd_dscrt` varchar(200) DEFAULT NULL COMMENT '속성코드설명',
+  `disp_yn` char(1) CHARACTER SET latin1 NOT NULL DEFAULT 'Y' COMMENT '디스플레이여부',
+  `use_yn` char(1) CHARACTER SET latin1 NOT NULL DEFAULT 'Y' COMMENT '사용여부',
+  `ord_num` smallint(5) DEFAULT NULL COMMENT '정렬번호',
+  `crt_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시',
   `udt_dt` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `upt_uid` varchar(15) DEFAULT NULL,
+  `upt_uid` varchar(15) DEFAULT NULL COMMENT '수정자ID',
   PRIMARY KEY (`cd_seq`,`atcd`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='코드속성';
 
 CREATE TABLE `cm_cd_grp` (
-  `grp_cd` varchar(4) NOT NULL,
-  `grp_nm` varchar(50) NOT NULL,
-  `grp_dscrnm` varchar(200) DEFAULT NULL,
-  `use_yn` char(1) NOT NULL DEFAULT 'Y',
-  `crt_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `grp_cd` varchar(4) NOT NULL COMMENT '코드그룹 코드',
+  `grp_nm` varchar(50) NOT NULL COMMENT '코드그룹명',
+  `grp_dscrt` varchar(200) DEFAULT NULL COMMENT '코드그룹 설명',
+  `use_yn` char(1) CHARACTER SET latin1 NOT NULL DEFAULT 'Y' COMMENT '사용여부',
+  `crt_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시',
   `udt_dt` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `p_grp_cd` varchar(4) DEFAULT NULL,
+  `p_grp_cd` varchar(4) DEFAULT NULL COMMENT '상위그룹코드',
   PRIMARY KEY (`grp_cd`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='코드그룹';
 
 CREATE TABLE `cm_cd_inf` (
-  `cd_seq` int(11) NOT NULL,
-  `grp_cd` varchar(4) NOT NULL,
-  `lang_cd` varchar(4) NOT NULL,
-  `cd_dispnm` varchar(100) DEFAULT NULL,
-  `cd_dscrnm` varchar(200) DEFAULT NULL,
+  `cd_seq` int(11) NOT NULL COMMENT '코드순번',
+  `grp_cd` varchar(4) NOT NULL COMMENT '그룹코드',
+  `lang_cd` varchar(4) NOT NULL COMMENT '언어코드',
+  `cd_dispnm` varchar(100) DEFAULT NULL COMMENT '디스플레이명',
+  `cd_dscrnm` varchar(200) DEFAULT NULL COMMENT '코드설명',
   PRIMARY KEY (`grp_cd`,`lang_cd`),
   UNIQUE KEY `cd_seq` (`cd_seq`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='공통코드정보';
 
 CREATE TABLE `cm_cd_lang` (
-  `lang_cd` varchar(4) NOT NULL,
-  `lang_nm` varchar(100) NOT NULL,
-  `crt_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `udt_dt` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `lang_cd` varchar(4) NOT NULL COMMENT '언어코드',
+  `lang_nm` varchar(100) NOT NULL COMMENT '언어명',
+  `crt_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시',
+  `udt_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시',
   PRIMARY KEY (`lang_cd`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='코드구분';
 
-CREATE TABLE `cm_cdgrp` (
-  `grp_cd` varchar(4) NOT NULL,
-  `grp_nm` varchar(50) NOT NULL,
-  `grp_dscrnm` varchar(200) DEFAULT NULL,
-  `use_yn` char(1) NOT NULL DEFAULT 'Y',
-  `crt_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `udt_dt` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `p_grp_cd` varchar(4) DEFAULT NULL,
-  PRIMARY KEY (`grp_cd`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='코드그룹';
-
 CREATE TABLE `cm_file_grp` (
-  `file_grp_seq` int(11) NOT NULL,
-  `file_seq` int(11) NOT NULL,
-  `file_nm` varchar(100) NOT NULL,
-  `file_size` smallint(11) DEFAULT NULL,
-  `crt_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `udt_dt` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `udt_uid` varchar(15) DEFAULT NULL,
+  `file_grp_seq` int(11) NOT NULL COMMENT '파일그룹순번',
+  `file_seq` int(11) NOT NULL COMMENT '파일순번',
+  `file_nm` varchar(100) NOT NULL COMMENT '파일명',
+  `file_size` smallint(11) DEFAULT NULL COMMENT '파일용량',
+  `crt_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시',
+  `udt_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시',
+  `udt_uid` varchar(15) DEFAULT NULL COMMENT '수정자ID',
   PRIMARY KEY (`file_grp_seq`,`file_seq`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='첨부파일그룹';
 
--- note/schema/schema_sbm.dealder.sql
+++ note/schema/schema_sbm.dealder.sql
@@ -167,3 +167,8 @@ CREATE TABLE `om_worker` (
   PRIMARY KEY (`worker_uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='담당자정보';
 
+
+create or replace view v_cm_cd_inf AS 
+select c.cd_seq,c.grp_cd,g.grp_nm,g.p_grp_cd,c.lang_cd,t.lang_nm,c.cd_dispnm,c.cd_dscrnm
+from ((cm_cd_grp g join cm_cd_lang t on g.use_yn='Y') join cm_cd_inf c) where ((c.grp_cd = g.grp_cd) and (c.lang_cd = t.lang_cd));
+
