4d6313efe9ee2bcb82e88e503e08a19b5502b3e5
KPBAEK <KPBAEK@sbmkorea.com>
2014-08-12 17:43:42 +0900

    .

---
+++ note/schema/om_team.sql
@@ -0,0 +1,14 @@
+USE sbm;
+
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600FN0','team_finance@sbmkorea.com','chonj@sbmkorea.com','N');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600MF0','team_manufacture@sbmkorea.com','lksdream@sbmkorea.com','N');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600MT0','team_mtech@sbmkorea.com','lapis76k@sbmkorea.com','N');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600PC0','team_procurement@sbmkorea.com','wch7519@sbmkorea.com','N');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600QC0','team_qc@sbmkorea.com','bungzo@sbmkorea.com','N');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600SL0','team_sales@sbmkorea.com','arthurkim@sbmkorea.com','N');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600ST0','team_strategy@sbmkorea.com','mireo1@sbmkorea.com','N');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RHW0','team_hw@sbmkorea.com','mskim@sbmkorea.com','N');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RMC0','team_mechanics@sbmkorea.com','cadwon@sbmkorea.com','N');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RSW1','team_sw1@sbmkorea.com','mnsong@sbmkorea.com','N');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RSW2','team_sw2@sbmkorea.com','joshin74@sbmkorea.com','N');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RSW3','team_sw3@sbmkorea.com','jpark@sbmkorea.com','N');
