86f59075927eac9aac9ed2afa7a871d8343db710
KPBAEK <KPBAEK@sbmkorea.com>
2014-08-12 18:37:34 +0900

    .

---
+++ note/schema/om_admin.sql
@@ -0,0 +1,11 @@
+USE sbm;
+
+insert into `om_admin`(`adm_uid`,`adm_pswd`,`auth_grp_cd`,`perms_cd`,`active_yn`,`last_logindt`,`crt_dt`,`udt_dt`) values ('arthurkim','sbm123','DW',null,'Y','2014-08-12 18:33:53','2014-08-12 18:33:53','2014-08-12 18:33:53');
+insert into `om_admin`(`adm_uid`,`adm_pswd`,`auth_grp_cd`,`perms_cd`,`active_yn`,`last_logindt`,`crt_dt`,`udt_dt`) values ('chang2581','sbm123','DW',null,'Y','2014-08-12 18:33:53','2014-08-12 18:33:53','2014-08-12 18:33:53');
+insert into `om_admin`(`adm_uid`,`adm_pswd`,`auth_grp_cd`,`perms_cd`,`active_yn`,`last_logindt`,`crt_dt`,`udt_dt`) values ('jackyun','sbm123','DW',null,'Y','2014-08-12 18:33:53','2014-08-12 18:33:53','2014-08-12 18:33:53');
+insert into `om_admin`(`adm_uid`,`adm_pswd`,`auth_grp_cd`,`perms_cd`,`active_yn`,`last_logindt`,`crt_dt`,`udt_dt`) values ('ju.lee','sbm123','DW',null,'Y','2014-08-12 18:33:53','2014-08-12 18:33:53','2014-08-12 18:33:53');
+insert into `om_admin`(`adm_uid`,`adm_pswd`,`auth_grp_cd`,`perms_cd`,`active_yn`,`last_logindt`,`crt_dt`,`udt_dt`) values ('ktpark','sbm123','DW',null,'Y','2014-08-12 18:33:53','2014-08-12 18:33:53','2014-08-12 18:33:53');
+insert into `om_admin`(`adm_uid`,`adm_pswd`,`auth_grp_cd`,`perms_cd`,`active_yn`,`last_logindt`,`crt_dt`,`udt_dt`) values ('peterkang','sbm123','DW',null,'Y','2014-08-12 18:33:53','2014-08-12 18:33:53','2014-08-12 18:33:53');
+insert into `om_admin`(`adm_uid`,`adm_pswd`,`auth_grp_cd`,`perms_cd`,`active_yn`,`last_logindt`,`crt_dt`,`udt_dt`) values ('sooj','sbm123','DW',null,'Y','2014-08-12 18:33:53','2014-08-12 18:33:53','2014-08-12 18:33:53');
+insert into `om_admin`(`adm_uid`,`adm_pswd`,`auth_grp_cd`,`perms_cd`,`active_yn`,`last_logindt`,`crt_dt`,`udt_dt`) values ('splashp','sbm123','DW',null,'Y','2014-08-12 18:33:53','2014-08-12 18:33:53','2014-08-12 18:33:53');
+insert into `om_admin`(`adm_uid`,`adm_pswd`,`auth_grp_cd`,`perms_cd`,`active_yn`,`last_logindt`,`crt_dt`,`udt_dt`) values ('yh7355','sbm123','DW',null,'Y','2014-08-12 18:33:53','2014-08-12 18:33:53','2014-08-12 18:33:53');
--- note/schema/om_team.sql
+++ note/schema/om_team.sql
@@ -7,6 +7,7 @@ insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_y
 insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600QC0','team_qc@sbmkorea.com','bungzo@sbmkorea.com','N');
 insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600SL0','team_sales@sbmkorea.com','arthurkim@sbmkorea.com','N');
 insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('00600ST0','team_strategy@sbmkorea.com','mireo1@sbmkorea.com','N');
+insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060HQ00','team_heads@sbmkorea.com',null,'N');
 insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RHW0','team_hw@sbmkorea.com','mskim@sbmkorea.com','N');
 insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RMC0','team_mechanics@sbmkorea.com','cadwon@sbmkorea.com','N');
 insert into `om_team`(`team_atcd`,`team_email`,`team_mngr_email`,`team_mailing_yn`) values ('0060RSW1','team_sw1@sbmkorea.com','mnsong@sbmkorea.com','N');
