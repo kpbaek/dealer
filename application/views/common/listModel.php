10cc2b9d4ecc5ea5e7b68e3e38d15c21336e07c1
KPBAEK <KPBAEK@sbmkorea.com>
2014-08-21 12:19:17 +0900

    .

--- application/views/common/listModel.php
+++ application/views/common/listModel.php
@@ -1,5 +1,5 @@
 <?php
-$cd = $_REQUEST["cd"];
+$atcd = $_REQUEST["atcd"];
 
 // include db config
 include_once("/config.php");
@@ -8,7 +8,8 @@ include_once("/config.php");
 $db = mysql_connect(PHPGRID_DBHOST, PHPGRID_DBUSER, PHPGRID_DBPASS);
 mysql_select_db(PHPGRID_DBNAME);
 
-$SQL = "SELECT cd, atcd, atcd_nm, USE_YN FROM cm_cd_attr a WHERE a.use_yn = 'Y' and disp_yn = 'Y' and a.cd='".$cd. "' order by ord_num";
+#$SQL = "SELECT cd, atcd, atcd_nm, USE_YN FROM cm_cd_attr a WHERE a.use_yn = 'Y' and disp_yn = 'Y' and a.cd=".$cd. " order by ord_num";
+$SQL = "SELECT 'model' cd, mdl_cd atcd, mdl_nm atcd_nm FROM om_mdl a WHERE 1=1 order by mdl_cd";
 $result = mysql_query( $SQL ) or die("Couldn t execute query.".mysql_error());
 
 $responce = "";
