c3439994f437da3b154195c526e633742d8295c0
KPBAEK <KPBAEK@sbmkorea.com>
2014-08-22 11:51:33 +0900

    .

---
+++ include/email/00700111.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	
+	<style type="text/css">
+	  html { font-family:Calibri, Arial, Helvetica, sans-serif; font-size:11pt; background-color:white }
+	  table { border-collapse:collapse; page-break-after:always; }
+	  .gridlines td { border:1px dotted black }
+	  .b { text-align:center }
+	  .e { text-align:center }
+	  .f { text-align:right }
+	  .inlineStr { text-align:left }
+	  .n { text-align:right }
+	  .s { text-align:left }
+	  td.style01 { vertical-align:middle; text-align:center; padding-left:0px; border-bottom:none #000000; border-top:1px solid #000000 !important; border-left:1px solid #000000 !important; border-right:none #000000; font-weight:bold; color:#000000; font-family:'Calibri'; font-size:11pt; background-color:#A0A0A0 }
+	  td.style02 { vertical-align:middle; text-align:center; padding-left:0px; border-bottom:none #000000; border-top:1px solid #000000 !important; border-left:1px solid #000000 !important; border-right:none #000000; font-weight:bold; color:#000000; font-family:'Calibri'; font-size:11pt; background-color:#D0A0A0 }
+	  table.sheet0 col.col0 { width:42pt }
+	  table.sheet0 col.col1 { width:42pt }
+	  table.sheet0 col.col2 { width:42pt }
+	  table.sheet0 col.col3 { width:42pt }
+	  table.sheet0 col.col4 { width:42pt }
+	  table.sheet0 col.col5 { width:42pt }
+	  table.sheet0 col.col6 { width:42pt }
+	  table.sheet0 col.col7 { width:82pt }
+	  table.sheet0 col.col8 { width:82pt }
+	  table.sheet0 tr { height:15pt }
+	</style>	
+		
+		
+<table border="0" cellpadding="1" cellspacing="1" id="sheet0" width="950">
+		<col class="col0">
+		<col class="col1">
+		<col class="col2">
+		<col class="col3">
+		<tbody>
+		  <tr class="row0" bgcolor="white">
+			<td colspan=10 class="style01">Purchase Order</td>
+		  </tr>
+		  <tr height="5px">
+			<td width="15%" class="style01" colspan=2>Date</td>
+			<td width="10%">2004-03-14</td>
+			<td width="10%"></td>
+			<td width="10%"></td>
+			<td width="15%" class="style01">Dest Country</td>
+			<td width="10%" colspan=3>Democratic Republic of the Congo (Kinshasa)	
+			</td>
+			<td width="5%"></td>
+		  </tr>
+		  <tr>
+			<td class="style01" colspan=2>Company Name</td>
+			<td colspan=3>Kosovo - Treo</td>
+			<td class="style01">Model</td>
+			<td width="10%">SB-2000
+			</td>
+			<td colspan="3"></td>
+		  </tr>
+		  <tr>
+		  	<td class="style01" colspan=2>No.</td>
+			<td>SWM-1204-07</td>
+			<td colspan=2></td>
+			<td class="style01">P/O NO.</td>
+			<td>15220</td>
+			<td colspan=3></td>
+		  </tr>
+		  <tr>
+			<td class="style01" colspan=2>P/I NO. </td>
+			<td>PI-120403-2</td>
+			<td colspan=2></td>
+			<td class="style01">Q'TY</td>
+			<td>20</td>
+			<td colspan=3></td>
+		  </tr>
+		  <tr>
+			<td rowspan="2" class="style01" colspan=2>Currency</td>
+		  	<td colspan=8>RSD EUR USD GBP CHF RSD EUR USD GBP CHF RSD EUR USD GBP CHF </td>
+		  </tr>
+		  <tr>
+			<td colspan=3>
+			    <div class="form-group">
+			    </div>
+			</td>
+			<td class="style01">Serial Number</td>
+			<td class="style01">P-OCR
+			</td>
+			<td colspan=3>RSD EUR USD GBP CHF RSD EUR USD GBP CHF RSD EUR USD GBP CHF
+			</td>
+		  </tr>
+		  <tr>
+			<td class="style01" colspan=2>LCD Color</td>
+			<td colspan=3>Mono Chrom
+			</td>
+			<td class="style01">LCD Language</td>
+			<td>English
+			</td>
+		   	<td colspan=4></td>
+		  </tr>
+		  <tr>
+			<td class="style01" colspan=2>Reject Pocket Type</td>
+			<td colspan=3>A (Hard Type for Most Countries)
+			</td>
+			
+			<td colspan=5></td>
+		  </tr>
+		  <tr class="row18">
+			<td class="style01" colspan=2>Power Cable</td>
+			<td colspan=3>Micro USB to Serial
+    		</td>
+			<td class="style01">Other Options</td>
+			<td colspan=4>SV-200, SDP-7, Parallel Board
+			</td>
+		  </tr>
+		  <tr>
+			<td class="style01" colspan=2>Accessaries<br>(Bill Guide, Brush)</td>
+			<td class="style02">Serial Printer Cable</td>
+			<td colspan=2>O
+			</td>
+			<td class="style02">Calibration Sheet</td>
+			<td>X
+			</td>
+			<td class="style02">PC Cable</td>
+			<td>O
+			</td>
+			<td colspan=2></td>
+		  </tr>
+		  <tr>
+			<td class="style01" colspan=2>Shipped by</td>
+			<td colspan=3>courrier FedEx
+			</td>
+			<td class="style01">Account no</td>
+			<td></td>
+			<td colspan=5>1111</td>
+		  </tr>
+		  <tr>
+			<td class="style01" colspan=2>Delivery</td>
+			<td colspan=3>2014-08-21</td>
+			<td class="style01">Payment</td>
+			<td>T/T in advance
+			</td>
+			<td class="style01">Incoterms</td>
+			<td>Ex-work
+			</td>
+		   	<td colspan=3></td>
+		  </tr>
+		  <tr>
+			<td class="style01" colspan=2>Remark</td>
+			<td colspan=9>SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ</td>
+		  </tr>
+		  <tr height=5px>
+			<td colspan=10></td>
+		  </tr>
+		</tbody>
+	</table>
+
+
+
+
