0bbbcdf7f31c5c4ec6329d0c4325bf65b13fb04c
KPBAEK <KPBAEK@sbmkorea.com>
2014-08-25 18:09:26 +0900

    .

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,5 +1,4 @@
 
-
 <div id="box">
 
 <form  id="loginForm" name="loginForm" method="post" action='login_ok.php'>
@@ -7,11 +6,11 @@
 <tr>
         <td>아이디</td>
         <td style='width:10px'></td>
-        <td><input type='text' name='user_id' size="15" style='width:130px' tabindex='1'/></td>
+        <td><input type='text' id="uid" name='uid' size="15" style='width:130px' tabindex='1'/></td>
         <td style='width:20px'></td>
         <td rowspan='3'>
         <input id="login" type='button' tabindex='3' value='로그인' style='width:70px;height:50px'/>
-        <input type='button' tabindex='4' value='권한대행' style='width:70px;height:50px'/>
+        <input id="login2" type='button' tabindex='4' value='권한대행' style='width:70px;height:50px'/>
         </td>
 </tr>
 <tr>
@@ -20,7 +19,7 @@
 <tr>
         <td>비밀번호</td>
         <td style='width:10px'></td>
-        <td><input type='password' name='user_pw' size="15" style='width:130px' tabindex='2'/></td>
+        <td><input type='password' id="pswd" name='pswd' size="15" style='width:130px' tabindex='2'/></td>
         <td style='width:30px'></td>
 </tr>
 <tr>
@@ -37,49 +36,93 @@
 	<td>권한대행 아이디: 바이어의 email</td>
 </tr>
 </table>
-</body>
 
-	<script>
-			$(document).ready(function() {
-                
-   			 $('#login').click(function()
-			{
-//                 $('#box').shake();
-       			 $("#error").html("<span style='color:#cc0000'>Error:</span> Invalid userid and password. ");
-			/**
-			var username=$("#username").val();
-			var password=$("#password").val();
-		    var dataString = 'username='+username+'&password='+password;
-			if($.trim(username).length>0 && $.trim(password).length>0)
-			{
-			
- 
+<script>
+$(document).ready(function() {
+    
+	$('#login').click(function()
+	{
+		var uid=$("#uid").val();
+		var pswd=$("#pswd").val();
+//		var uid="admin";
+//		var pswd="admin123";
+		
+		if($.trim(uid).length==0)
+		{
+        	$('#box').shake();
+			$("#error").html("<span style='color:#cc0000'>Error:</span> user ID is required. ");
+			return;
+		}
+		if($.trim(pswd).length==0)
+		{
+        	$('#box').shake();
+			$("#error").html("<span style='color:#cc0000'>Error:</span> password is required. ");
+			return;
+		}
+		if($.trim(uid).length>0 && $.trim(pswd).length>0)
+		{
 			$.ajax({
-            type: "POST",
-            url: "ajaxLogin.php",
-            data: dataString,
-            cache: false,
-            beforeSend: function(){ $("#login").val('Connecting...');},
-            success: function(data){
-            if(data)
-            {
-            $("body").load("home.php").hide().fadeIn(1500).delay(6000);
-			}
-            else
-            {
-             $('#box').shake();
-			 $("#login").val('Login')
-			 $("#error").html("<span style='color:#cc0000'>Error:</span> Invalid username and password. ");
-            }
-            }
-            });
-			
-			}
-   			*/			
-			return false;
+			        type: "POST",
+			        url: "/user/ajaxLogin",
+			        data: {"uid":uid, "pswd":pswd},
+			        cache: false,
+			        beforeSend: function(){ $("#login").val('Connecting...');},
+			        success: function(result){
+				        if(result)
+				        {
+//				        	$("#error").text(result);
+					        location.replace("/admin");
+						}
+				        else
+				        {
+				        	$('#box').shake();
+							$("#login").val('로그인');
+							$("#error").html("<span style='color:#cc0000'>Error:</span> Invalid user ID and password. ");
+				        }
+			        }
 			});
-			
-   			 
+		
+		}
+	
+	});
+
+	$('#login2').click(function()
+	{
+		var uid= $("#uid").val();
+//		var pswd=$("#pswd").val();
+		var pswd="dealer123";
+	
+		if($.trim(uid).length==0)
+		{
+        	$('#box').shake();
+			$("#error").html("<span style='color:#cc0000'>Error:</span> user ID is required. ");
+			return;
+		}
+		if($.trim(uid).length>0 && $.trim(pswd).length>0)
+		{
+			$.ajax({
+			        type: "POST",
+			        url: "/user/ajaxLogin",
+			        data: {"uid":uid, "pswd":pswd},
+			        cache: false,
+			        beforeSend: function(){ $("#login").val('Connecting...');},
+			        success: function(data){
+				        if(data)
+				        {
+					        location.replace("/admin");
+						}
+				        else
+				        {
+				        	$('#box').shake();
+							$("#login").val('로그인');
+							$("#error").html("<span style='color:#cc0000'>Error:</span> Invalid user ID. ");
+				        }
+			        }
 			});
-			
-		</script>
+		
+		}
+	
+	});	
+	
+});
+</script>
